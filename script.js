4899206ca0c3ddc00213240dd6a1e9326987356e
daliH-4cls <daliah2rnndz10@gmail.com>
2026-04-10 15:43:22 +0000

    Other html and javascript added

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log("Script for INDEX file starts here");
+// Only 1 of the lessons is selectable for easy coding, all are just setups
+
+// For Beginner Phone Conversation
+function learnPhoneConverse() {
+    console.log("click");
+    // No code yet
+}
+
+// Learning options for Phone Conversation
+function learnTakeOrders() {
+    //No code yet
+}
+
+function learnDescribeBusiness() {
+    //No Code yet
+}
+
+// Warning/ message for N/A lessons because I ain't doing alat
+function unselectableLessons() {
+    // No code yet
+}
