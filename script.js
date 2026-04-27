9a3ed84e4414878a441e815d66ec1d3e0f6758bd
daliH-4cls <daliah2rnndz10@gmail.com>
2026-04-27 15:42:33 +0000

    Small updates

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 console.log("Script for INDEX file starts here");
 // Only 1 of the lessons is selectable for easy coding, all are just setups
 
+// Storing user answers
+let userAnswer = "";
+
 // For Beginner Phone Conversation
 function learnPhoneConverse() {
     console.log("click");
     // No code yet
 }
 
-// Learning options for Phone Conversation
+// Learning option 1 for Phone Conversation
 function learnTakeOrders() {
     //No code yet
 }
 
+// Learning option 2
 function learnDescribeBusiness() {
     //No Code yet
 }
